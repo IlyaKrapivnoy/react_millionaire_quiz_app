dea68b3a21607eccbad859257c7dacbdb3da1fbf
ilya.krapivnoy <ilya.krapivnoy@gmail.com>
2021-08-31 15:16:27 +0300

    add design to main part

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import './app.css';
+
 function App() {
-    return <div>test</div>;
+    return (
+        <div className='app'>
+            <div className='main'>main</div>
+            <div className='pyramid'>money</div>
+        </div>
+    );
 }
 
 export default App;
