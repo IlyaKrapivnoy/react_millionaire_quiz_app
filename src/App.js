087eb90ca75086fd292bdcd868ca74f884a85e6d
ilya.krapivnoy <ilya.krapivnoy@gmail.com>
2021-08-31 15:25:04 +0300

    add pyramid section design

--- src/App.js
+++ src/App.js
@@ -4,7 +4,26 @@ function App() {
     return (
         <div className='app'>
             <div className='main'>main</div>
-            <div className='pyramid'>money</div>
+            <div className='pyramid'>
+                <ul className='moneyLIst'>
+                    <li className='moneyListItem active'>
+                        <span className='moneyListItemNumber'>4</span>
+                        <span className='moneyListItemAmount'>$ 400</span>
+                    </li>
+                    <li className='moneyListItem'>
+                        <span className='moneyListItemNumber'>3</span>
+                        <span className='moneyListItemAmount'>$ 300</span>
+                    </li>
+                    <li className='moneyListItem'>
+                        <span className='moneyListItemNumber'>2</span>
+                        <span className='moneyListItemAmount'>$ 200</span>
+                    </li>
+                    <li className='moneyListItem'>
+                        <span className='moneyListItemNumber'>1</span>
+                        <span className='moneyListItemAmount'>$ 100</span>
+                    </li>
+                </ul>
+            </div>
         </div>
     );
 }
