dc080ba593c61a1edf1be5fbc034ec8a32f315e1
ilya.krapivnoy <ilya.krapivnoy@gmail.com>
2021-08-31 16:23:25 +0300

    add Trivia component

---
+++ src/components/Trivia.jsx
@@ -0,0 +1,15 @@
+const Trivia = () => {
+    return (
+        <div className='trivia'>
+            <div className='question'>Who is the most popular author?</div>
+            <div className='answers'>
+                <div className='answer'>David Flanagan</div>
+                <div className='answer'>Scott Grahnnaman</div>
+                <div className='answer'>Ban Ahlbahary</div>
+                <div className='answer'>Stephen King</div>
+            </div>
+        </div>
+    );
+};
+
+export default Trivia;
